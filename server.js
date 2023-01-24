f2b11eb998d204b28825875b557a0084bc96ae34
Naadir Mohamed <smartnaadir@gmail.com>
2023-01-24 10:07:12 +0400

    Main commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+// Routes GET requests to /lessons to the request handler
+app.get("/lessons", function(request, response) {
+    response.json( [{topic: 'Math',location:'Hendon',price:100},
+                    {topic: 'Math',location:'Colindale',price:80},
+                    {topic: 'Math',location:'Brent Cross',price:90},
+                    {topic: 'Math',location:'Golden Green',price:120}]);
+    });
+
+// Routes GET requests to /lessons to the request handler
+app.get("/user", function(request, response) 
+    {response.json({email:"abc@gmail.com",password:"password123"});});
+
+// If it is not a GET request, also servers a 404 error.
+app.use(function(request, response) {response.status(404).send("Page not found!");});
+
+// Starts the server on port 3000
+app.listen(3000);
